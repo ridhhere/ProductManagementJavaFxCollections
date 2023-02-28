a5f902b938253f776df22126f9f3f182c2ddda5d
ridhhere <ridhhere@gmail.com>
2023-02-28 17:00:07 -0500

    cleanup

--- src/com/pms/client/ProductClientGUI.java
+++ src/com/pms/client/ProductClientGUI.java
@@ -1,41 +1,23 @@
 
 package com.pms.client;
 
-import com.pms.details.ProductDetails;
 import com.pms.details.ProductDetailsGUI;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class ProductClientGUI extends Application {
 	ProductDetailsGUI productDetailsGui = new ProductDetailsGUI();
-	private static final int INACTIVITY_TIMEOUT = 5 * 60 * 1000; // 5 minutes in milliseconds
-
-	private long lastInteractionTime;
-
 	@Override
 	public void start(Stage stage) {
 		// Set the title of the window to "Product Management System"
